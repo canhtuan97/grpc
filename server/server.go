ed681c5a4417c427a710cebe0afcaf0b0117f671
canhtuan97 <canhtuan97@gmail.com>
2020-07-23 09:51:06 +0700

    asd

--- server/server.go
+++ server/server.go
@@ -1,11 +1,25 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
 	"fmt"
+	"github.com/canhtuan97/grpc/proto"
 	"github.com/canhtuan97/grpc/structs"
+	"google.golang.org/grpc"
 	"log"
+	"net"
 )
+
+type server struct{}
+
+func (*server) GetPokemon(ctx context.Context, req *app_serverpb.MoveRequest) (*app_serverpb.MoveResponse, error) {
+	panic("implement me")
+}
+
+//func (s server) GetPokemon(ctx context.Context, request *app_serverpb.MoveRequest) (*app_serverpb.MoveResponse, error) {
+//	panic("implement me")
+//}
 
 func Test()  {
 	fmt.Printf("Starting server at port 8080\n")
@@ -20,6 +34,19 @@ func Test()  {
 }
 func main() {
 
+	lis ,err := net.Listen("tpc","0.0.0.0:50069")
+	if err != nil {
+		log.Fatalf("err while create listen %v", err)
+	}
 
+	s := grpc.NewServer()
 
+	app_serverpb.RegisterPokemonServiceServer(s , &server{})
+
+	fmt.Println("Server running ...")
+
+	err = s.Serve(lis)
+	if err != nil {
+		log.Fatalf("err while serve %v", err)
+	}
 }
